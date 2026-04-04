9cb37a9cb3344774431613183a660ca9dadefaca
cristian <cristian.x.ruvalcaba@gmail.com>
2026-04-04 08:37:34 -0700

    fix: force dynamic render by reading headers() in trial page

--- portal/src/app/trial/page.tsx
+++ portal/src/app/trial/page.tsx
@@ -1,8 +1,12 @@
+import { headers } from "next/headers";
 import TrialForm from "./TrialForm";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-export default function TrialPage() {
+export default async function TrialPage() {
+  // Force dynamic rendering by reading request headers
+  const h = await headers();
+  void h.get("x-request-id");
   return <TrialForm />;
 }
